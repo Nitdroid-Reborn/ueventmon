300507e7d685f500ed4ceb755f28aebc0ab2443e
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2013-05-26 13:47:19 +0400

    Fix code style and compilation for latest AOSP

--- ueventmon.c
+++ ueventmon.c
@@ -32,81 +32,81 @@
 /* Returns -1 on failure, and socket fd on success */
 static int open_uevent()
 {
-	struct sockaddr_nl addr;
-	int sz = UEVENT_BUFFER_SIZE;
-	int s;
+    struct sockaddr_nl addr;
+    int sz = UEVENT_BUFFER_SIZE;
+    int s;
 
-	memset(&addr, 0, sizeof(addr));
-	addr.nl_family = AF_NETLINK;
-	addr.nl_groups = 0xffffffff;
+    memset(&addr, 0, sizeof(addr));
+    addr.nl_family = AF_NETLINK;
+    addr.nl_groups = 0xffffffff;
 
-	/*
-	*	netlink(7) on nl_pid:
- 	*	If the application sets it to 0, the kernel takes care of assigning it.
-	*	The  kernel assigns the process ID to the first netlink socket the process
-	*	opens and assigns a unique nl_pid to every netlink socket that the
-	*	process subsequently creates.
-	*/
-	addr.nl_pid = getpid();
+    /*
+     *  netlink(7) on nl_pid:
+     *  If the application sets it to 0, the kernel takes care of assigning it.
+     *  The  kernel assigns the process ID to the first netlink socket the process
+     *  opens and assigns a unique nl_pid to every netlink socket that the
+     *  process subsequently creates.
+     */
+    addr.nl_pid = getpid();
 
-	s = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
-	if(s < 0) {
-		LOGE("%s socket failed: %s", __func__, strerror(errno));
-		return -1;
-	}
+    s = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
+    if(s < 0) {
+        printf("%s socket failed: %s", __func__, strerror(errno));
+        return -1;
+    }
 
-	setsockopt(s, SOL_SOCKET, SO_RCVBUFFORCE, &sz, sizeof(sz));
+    setsockopt(s, SOL_SOCKET, SO_RCVBUFFORCE, &sz, sizeof(sz));
 
-	if(bind(s, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-		LOGE("%s bind failed: %s", __func__, strerror(errno));
-		close(s);
-		return -1;
-	}
+    if(bind(s, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        printf("%s bind failed: %s", __func__, strerror(errno));
+        close(s);
+        return -1;
+    }
 
 
-	return s;
+    return s;
 }
 
 int uevent_next_event(int fd, char* buffer, int buffer_length)
 {
-	while (1) {
-		struct pollfd fds;
-		int nr;
+    while (1) {
+        struct pollfd fds;
+        int nr;
 
-		fds.fd = fd;
-		fds.events = POLLIN;
-		fds.revents = 0;
-		nr = poll(&fds, 1, -1);
+        fds.fd = fd;
+        fds.events = POLLIN;
+        fds.revents = 0;
+        nr = poll(&fds, 1, -1);
      
-		if (nr > 0 && fds.revents == POLLIN) {
-			int count = recv(fd, buffer, buffer_length, 0);
-			if (count > 0) {
-				return count;
-			}
-			LOGE("%s recv failed: %s", __func__, strerror(errno));
-		}
-	}
+        if (nr > 0 && fds.revents == POLLIN) {
+            int count = recv(fd, buffer, buffer_length, 0);
+            if (count > 0) {
+                return count;
+            }
+            printf("%s recv failed: %s", __func__, strerror(errno));
+        }
+    }
     
-	// won't get here
-	return 0;
+    // won't get here
+    return 0;
 }
 
 int main(void)
 {
-	int fd = open_uevent();
-	if (fd < 0)
-		return -1;
+    int fd = open_uevent();
+    if (fd < 0)
+        return -1;
 
-	printf("Socket opened\n");
+    printf("Socket opened\n");
 
-	static char buf[UEVENT_BUFFER_SIZE];
-	while(1) {
-		int count = uevent_next_event(fd, buf, sizeof(buf));
-		if (!count)
-			return -1;
-		printf("%s\n", buf);
-	}
+    static char buf[UEVENT_BUFFER_SIZE];
+    while(1) {
+        int count = uevent_next_event(fd, buf, sizeof(buf));
+        if (!count)
+            return -1;
+        printf("%s\n", buf);
+    }
 
-	return 0;
+    return 0;
 }
 
